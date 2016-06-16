4b7dbbfc8cd4331c4d07f8620c6a281bdeb5afc8
Alex Dekau <alex.potatofarms@gmail.com>
2016-06-16 01:28:03 -0400

    Fix route data loading

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,16 +1,88 @@
 <template>
-  <div class="server-select mdl-shadow--2dp">
-    <div class="server-select-container">
-      <strong>Server: &nbsp;</strong>
-      <select class="map-select" v-model="selectedWorld">
-        <option v-for="world in worldlist">
-          {{world.name}}
-        </option>
-      </select>
+  <div>
+    <div class="server-select mdl-shadow--2dp">
+      <div class="server-select-container">
+        <strong>Server: &nbsp;</strong>
+        <select class="map-select" v-model="selectedWorld">
+          <option v-for="world in worldlist">
+            {{world.name}}
+          </option>
+        </select>
+      </div>
     </div>
+    <div id="map"></div>
   </div>
-  <div id="map"></div>
 </template>
+
+<script>
+  import store from '../store'
+
+  export default {
+    data () {
+      return {
+        map: false,
+        worldlist: [],
+        selectedWorld: '',
+        matchArr: []
+      }
+    },
+
+    ready () {
+      var southWest, northEast
+
+      this.map = window.L.map('map', {
+        minZoom: 0,
+        maxZoom: 7,
+        crs: window.L.CRS.Simple,
+        maxBoundsViscosity: 1.0
+      }).setView([0, 0], 0)
+
+      southWest = this.unproject([0, 16384])
+      northEast = this.unproject([16384, 0])
+
+      this.map.setMaxBounds(new window.L.LatLngBounds(southWest, northEast))
+
+      window.L.tileLayer('https://tiles.guildwars2.com/2/1/{z}/{x}/{y}.jpg', {
+        minZoom: 0,
+        maxZoom: 7,
+        continuousWorld: true
+      }).addTo(this.map)
+    },
+
+    route: {
+      data ({ to }) {
+        return {
+          worldlist: store.fetchWorlds(),
+          matchArr: store.fetchMatches()
+        }
+      }
+    },
+
+    created () {
+      store.on('matches-updated', this.updateMatches)
+      store.on('worlds-updated', this.updateWorlds)
+    },
+
+    destroyed () {
+      store.removeListener('matches-updated', this.updateMatches)
+      store.removeListener('worlds-updated', this.updateWorlds)
+    },
+
+    methods: {
+      updateMatches () {
+        this.matchArr = store.fetchMatches()
+      },
+
+      updateWorlds () {
+        this.worldlist = store.fetchWorlds()
+      },
+
+      unproject: function (coord) {
+        return this.map.unproject(coord, this.map.getMaxZoom())
+      }
+    }
+  }
+</script>
 
 <style>
   .server-select {
@@ -44,6 +116,7 @@
     bottom: 0;
     right: 0;
     left: 0;
+    z-index: 1;
   }
 
   .maptimer {
@@ -54,65 +127,3 @@
     padding-bottom: 0;
   }
 </style>
-
-<script>
-  import store from '../store'
-
-  export default {
-    data () {
-      return {
-        map: false,
-        worldlist: [],
-        selectedWorld: '',
-        worldlist: [],
-        matchArr: []
-      }
-    },
-
-    ready () {
-      var southWest, northEast
-
-      this.map = window.L.map('map', {
-        minZoom: 0,
-        maxZoom: 7,
-        crs: window.L.CRS.Simple,
-        maxBoundsViscosity: 1.0
-      }).setView([0, 0], 0)
-
-      southWest = this.unproject([0, 16384])
-      northEast = this.unproject([16384, 0])
-
-      this.map.setMaxBounds(new window.L.LatLngBounds(southWest, northEast))
-
-      window.L.tileLayer('https://tiles.guildwars2.com/2/1/{z}/{x}/{y}.jpg', {
-        minZoom: 0,
-        maxZoom: 7,
-        continuousWorld: true
-      }).addTo(this.map)
-    },
-
-    created () {
-      store.on('matches-updated', this.updateMatches)
-      store.on('worlds-updated', this.updateWorlds)
-    },
-
-    destroyed () {
-      store.removeListener('matches-updated', this.updateMatches)
-      store.removeListener('worlds-updated', this.updateWorlds)
-    },
-
-    methods: {
-      updateMatches () {
-        this.matchArr = store.fetchMatches()
-      },
-
-      updateWorlds () {
-        this.worldlist = store.fetchWorlds()
-      },
-
-      unproject: function (coord) {
-        return this.map.unproject(coord, this.map.getMaxZoom())
-      }
-    }
-  }
-</script>
