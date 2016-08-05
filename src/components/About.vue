67d563209f6445909acb536fbb5fca43a10d47e2
Alex Dekau <alex.potatofarms@gmail.com>
2016-08-04 22:02:38 -0400

    Finally made about page

---
+++ src/components/About.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="mdl-shadow--2dp mdl-color--white mdl-cell mdl-cell--12-col">
+    <p class="aboutparagraph">
+      WvWStats is a statistics tracking website targeted at players of the World vs. World game mode
+      available in &nbsp;<a href="//www.guildwars2.com">Guild Wars 2</a>. It's features include but
+      are not limited to:
+      <ul class="aboutlist">
+        <li> Match tracking (NA and EU) </li>
+        <li> In depth match details </li>
+        <li> Stats by borderland </li>
+        <li> Server rating calculation </li>
+        <li> Live Map </li>
+        <li> Leaderboard </li>
+        <li> Grapher for a multitude of stats </li>
+        <li> Timezone strength analysis and statistics per server </li>
+      </ul>
+    </p>
+    <p class="aboutparagraph">
+      &copy; potatofarms.9241 of the Blackgate server 2016.
+    </p>
+  </div>
+</template>
+
+<style>
+  .aboutparagraph {
+    display:block;
+    margin: 10px;
+  }
+
+  .aboutlist li {
+    display: list-item;
+    list-style: circle;
+    margin-left: 52px;
+  }
+</style>
