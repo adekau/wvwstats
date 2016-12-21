b8990ea6c624763a6ad02dd2708a64f8bc78b954
Alex Dekau <alex.potatofarms@gmail.com>
2016-12-21 11:33:56 -0500

    Make component for MDL Buttons to allow them to get upgraded.

---
+++ src/components/MdlButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="mdl-button mdl-js-button mdl-color--blue-grey-100 mdl-js-ripple-effect">
+    {{text}}
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      text: {
+        type: String,
+        required: false
+      }
+    },
+
+    mounted () {
+      componentHandler.upgradeElement(this.$el, "MaterialButton")
+    }
+  }
+</script>
