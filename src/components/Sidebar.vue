ebf875e55ed77f450cdc4f68e6ec6ee7010e2783
Alex Dekau <alex.potatofarms@gmail.com>
2016-12-21 11:34:19 -0500

    Format links and re-implement map link with selectedWorld

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -7,14 +7,24 @@
     </header>
     </header>
     <nav class="demo-navigation mdl-navigation mdl-color--blue-grey-800">
-      <router-link to='/na' class="mdl-navigation__link"><i class="mdl-color-text--blue-grey-400 material-icons"
-                                                 role="presentation">list</i>NA Matches</router-link>
-      <router-link to='/eu' class="mdl-navigation__link"><i class="mdl-color-text--blue-grey-400 material-icons"
-                                                 role="presentation">list</i>EU Matches</router-link>
-      <!--<a v-bind:href="mapUrl" class="mdl-navigation__link"><i class="mdl-color-text--blue-grey-400 material-icons"
-                                                 role="presentation">autorenew</i>Live</a>-->
 
-      <router-link to='/map' class="mdl-navigation__link">
+      <router-link to='/na' class="mdl-navigation__link">
+        <i class="mdl-color-text--blue-grey-400 material-icons"
+          role="presentation">
+          list
+        </i>
+        NA Matches
+      </router-link>
+
+      <router-link to='/eu' class="mdl-navigation__link">
+        <i class="mdl-color-text--blue-grey-400 material-icons"
+          role="presentation">
+          list
+        </i>
+        EU Matches
+      </router-link>
+
+      <router-link :to="'/map/' + selectedWorld " class="mdl-navigation__link">
         <i class="mdl-color-text--blue-grey-400 material-icons"
           role="presentation">
           autorenew
@@ -22,74 +32,60 @@
         Live
       </router-link>
 
-      <router-link to='/leaderboard' class="mdl-navigation__link"><i class="mdl-color-text--blue-grey-400 material-icons"
-                                                 role="presentation">format_list_numbered</i>Leaderboard</router-link>
-      <router-link to="{ path: '/grapher' }" class="mdl-navigation__link">
+      <router-link to='/leaderboard' class="mdl-navigation__link">
+        <i class="mdl-color-text--blue-grey-400 material-icons"
+          role="presentation">
+          format_list_numbered
+        </i>
+        Leaderboard
+      </router-link>
+
+      <!--TODO: Get the grapher query thing back into this.-->
+      <router-link to="/grapher" class="mdl-navigation__link">
         <i class="mdl-color-text--blue-grey-400 material-icons"
           role="presentation">
           trending_up
         </i>
-          Grapher
+        Grapher
       </router-link>
 
-      <router-link to='/timezones' class="mdl-navigation__link"><i class="mdl-color-text--blue-grey-400 material-icons"
-                                                 role="presentation">schedule</i>Timezones</router-link>
+      <router-link to='/timezones' class="mdl-navigation__link">
+        <i class="mdl-color-text--blue-grey-400 material-icons"
+          role="presentation">
+          schedule
+        </i>
+        Timezones
+        </router-link>
       <router-link to='/embed' class="mdl-navigation__link">
         <i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">
           web
         </i>
         Embed
       </router-link>
+
       <div class="mdl-layout-spacer"></div>
-      <router-link to='/about' class="mdl-navigation__link"><i class="mdl-color-text--blue-grey-400 material-icons"
-                                                 role="presentation">info</i>About</router-link>
+
+      <router-link to='/about' class="mdl-navigation__link">
+        <i class="mdl-color-text--blue-grey-400 material-icons"
+          role="presentation">
+          info
+        </i>
+        About
+      </router-link>
+
     </nav>
   </div>
 </template>
 
 <script>
-  import store from '../store'
-
   export default {
-    data () {
-      return {
-        //selectedWorld: 0,
-        //grapherQuery: {server: null, data: null}
-      }
-    },
-
-    /*created () {
-      this.selectedWorld = store.fetchSelectedWorld()
-      this.grapherQuery = store.fetchGrapherQuery()
-
-      store.on('selectedWorld-updated', this.update)
-      store.on('grapherQuery-updated', this.updateGQ)
-    },
-
-    destroyed () {
-      store.removeListener('selectedWorld-updated', this.update)
-      store.removeListener('grapherQuery-updated', this.updateGQ)
-    },*/
+    data () { return {} },
 
     computed: {
-      mapUrl () {
-        if (this.selectedWorld == 0) {
-          return '#/map'
-        } else {
-          return '#/map/' + this.selectedWorld
-        }
+      selectedWorld () {
+        return this.$store.state.selectedWorld
       }
-    }//,
-
-    /*methods: {
-      update () {
-        this.selectedWorld = store.fetchSelectedWorld()
-      },
-
-      updateGQ () {
-        this.grapherQuery = store.fetchGrapherQuery()
-      }
-    }*/
+    }
   }
 </script>
 
